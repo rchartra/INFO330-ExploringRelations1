21f443b933de241aab0166312a05d1dddd8003ea
rchartra <113725205+rchartra@users.noreply.github.com>
2023-04-05 12:50:09 -0700

    Rename question_10.sql to question-10.sql

---
+++ question-10.sql
@@ -0,0 +1,1 @@
+INSERT INTO persons VALUES ('Robin', 'Chartrand', 'rchartra@uw.edu', 'Female', 'The Right Honourable', '19', '1234 Street Street', 'Seattle', 'Washington', '99999', '888-111-3333');
--- question_10.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO persons VALUES ('Robin', 'Chartrand', 'rchartra@uw.edu', 'Female', 'The Right Honourable', '19', '1234 Street Street', 'Seattle', 'Washington', '99999', '888-111-3333');
